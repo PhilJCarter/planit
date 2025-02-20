4e6495a9a08ecceadee3628deb944f407290c358
Phil Carter <carter.phil.j@gmail.com>
2025-02-20 18:38:10 +0000

    Clean up eos funcs; add bound remnant saving; fix potential calc; improve planet profile generation

--- main.py
+++ main.py
@@ -1,76 +1,66 @@
-### Module for accessing gadget data
-###
+"""
+planit  (PLANetary Impact Toolkit)
+======
 
-"""Classes and functions for accessing and manipulating Gadget data."""
+Classes and functions for setting-up, accessing, manipulating, 
+and analysing SPH impact simulation data.
 
-import numpy as npy
-import scipy
-import matplotlib
-import matplotlib.pyplot as plt
-import numba
-import struct
-import glob
+Supported planetary impact codes: gadget-2-planetary, swift
+
+
+Philip J. Carter (p.carter@bristol.ac.uk)
+
+Sarah T. Stewart (sts@sarahstewart.net)
+
+
+"""
+
 import os
-import inspect
-import h5py
+#import inspect
+#import h5py
 import woma
 
-eospath = os.path.expanduser('~') + '/Work/'
+eospath = os.path.dirname(__file__) + '/eos/data/'
+datadir = os.path.dirname(__file__) + '/data/'
 
-from makeplanets import eostable
-from .eos import EOStable, loadEOS
-#from .eos import eospath
+from . import eos
 
-GADGET_EOS_OFFSET = IDOFF = 200000000    # material id offset
-PROJ_ID_OFFSET = BODYOFF = 100000000     # body id offset
+G = 6.67E-8 # Gravitational constant  cm3/g/s2
 
-
-
-
-@numba.njit(parallel=True)
-def calc_potential(m,x,y,z):
-    G = 6.67e-8
-    pot = npy.zeros(len(m))
-    for j in numba.prange(len(m)):
-        pdist = npy.sqrt( (x-x[j])**2 + (y-y[j])**2 + (z-z[j])**2 )
-        pot[j] = npy.nansum(npy.where( npy.isinf(-G * m / pdist),0,-G * m / pdist ))
-    return pot
+GADGET_EOS_OFFSET = IDOFF   = 200000000    # material id offset
+PROJ_ID_OFFSET    = BODYOFF = 100000000    # body id offset
 
 
-IronEOS = loadEOS(eos='Iron-ANEOS-SLVTv0.2G1')
-AlloyEOS = loadEOS(eos='Fe85Si15-ANEOS-SLVTv0.2G1')
-ForsteriteEOS = loadEOS(eos='Forsterite-ANEOS-SLVTv1.0G1')
+IronEOS       = eos.ANEOSIron        # for backwards compatibility
+AlloyEOS      = eos.ANEOSFeSiAlloy
+ForsteriteEOS = eos.ANEOSForsterite
 
 
 
+def loadsnap(file, headonly = False, thermo=False, compress=False, mats=[402,400]):
+    s = Snapshot()
+    s.load(file, headonly=headonly, thermo=thermo, compress=compress, mats=mats)
+    return s
 
-if __name__ == "__main__":
-    import sys
-
-    file = 'snapshot_000'
-    if len(sys.argv) > 1:
-        file = sys.argv[1]
-
-    snap0 = Snapshot()
-    print(snap0.header.flag_entr_ics)
-
-    snap0.load(file, thermo=False)
-
-    print(snap0.header.flag_entr_ics, snap0.header.flag_metals)
-    #print file, snap0.U.max()/1.e11, 0.5*npy.sqrt(snap0.vx**2+snap0.vy**2+snap0.vx**2).max()
-
-
-    print(snap0.N)
-    #print snap0.header.num_files
-
-    #print snap0.id[:5], snap0.id[-5:], snap1.id[:5], snap1.id[-5:]
-    #print snap0.m[:5], snap0.m[-5:], snap1.m[:5], snap1.m[-5:]
-    #print snap0.x[:5], snap0.x[-5:], snap1.x[:5], snap1.x[-5:]
-    #print snap0.vx[:5], snap0.vx[-5:]
+def loadimpact(loc,thermo=False,inter=1,compress=True,code='swift'):
+    i = Impact()
+    i.load(loc, thermo=thermo, inter=inter, compress=compress, code=code)
+    return i
     
-    
-    #snap0.identify()
+def load_seagen(partplanet, thermo=False, init_h=100e5):
+    s = Snapshot()
+    s.load_seagen(partplanet, thermo=thermo, init_h=init_h)
+    return s
 
-    #plt.scatter(snap0.x[snap0.fors == 1], snap0.y[snap0.fors == 1], c='b')
-    #plt.scatter(snap0.x[snap0.iron == 1], snap0.y[snap0.iron == 1], c='orange')
-    #plt.show()
+def combine(body1, body2, bidoffset=PROJ_ID_OFFSET, thermo=False, box=0.0):
+    s = Snapshot()
+    s.combine(body1, body2, bidoffset=bidoffset, thermo=thermo, box=box)
+    return s
+
+def bound_mass(snapshot, nrem = 1, minbnd = 200, maxiter = 2000, tol = 0.01, reorder=True, discardsmall=False, calc_pot_all=True, save=True):
+    snapshot.bound_mass(nrem = nrem, minbnd = minbnd, maxiter = maxiter, tol = tol, reorder=reorder, discardsmall=discardsmall, calc_pot_all=calc_pot_all, save=save)
+    return snapshot.rem
+
+def calc_phase(snapshot, release=False, plot=False):
+    snapshot.calc_phase(release=release, plot=plot)
+    return snapshot.phase
---
+++ utils.py
@@ -0,0 +1,103 @@
+from .main import *
+import numpy as npy
+import numba
+
+
+class Node:
+    def __init__(self,width,origin,pos,mass,ids,leaves=[]):
+        self.size = width
+        self.origin = origin
+        self.children = []
+    
+        N = len(mass)
+        if N==1:
+            self.m = mass[0]
+            self.com = pos[0]
+            self.id = ids[0]
+            self.pot = 0
+            leaves.append(self)
+        elif N==2 and npy.sqrt(((pos[0]-pos[1])**2).sum())<(self.size/2e50):
+            self.m = mass.sum()
+            self.com = (pos[0]+pos[1])/2.
+            self.id = ids[0]
+            self.pot = 0
+            leaves.append(self)
+        elif self.size == 0.0:
+            if len(ids)>2:
+                print('WARNING: ', len(ids), ' overlapping particles: ',ids)
+            self.m = mass.sum()
+            self.com = pos[0]
+            self.id = ids[0]
+            self.pot = 0
+            leaves.append(self)
+        else:
+            self.genChild(width,origin,pos,mass,ids,leaves)
+        
+            m = [c.m for c in self.children]
+            com = [c.com for c in self.children]
+            m = npy.array(m)
+            com = npy.array(com)
+            self.m = m.sum()
+            self.com = (m*com.T).T.sum(axis=0)/self.m
+    
+    
+    def genChild(self,width,origin,pos,mass,ids,leaves):
+        oct_choose = pos > self.origin
+        oct_choose = oct_choose.astype(int)
+        for i in [0,1]:
+            for j in [0,1]:
+                for k in[0,1]:
+                    this_oct = (oct_choose[:,0] == i)*(oct_choose[:,1] == j)*(oct_choose[:,2] == k)
+                    if npy.any(this_oct):
+                        dw = 0.5*self.size*(npy.array([i,j,k])-0.5)
+                        self.children.append(Node(0.5*self.size,self.origin+dw,pos[this_oct],mass[this_oct],ids[this_oct],leaves))
+
+
+
+def Walk_Pot(node,leaf,theta2):
+    #G = 6.67e-8
+    dr = node.com - leaf.com
+    r2 = (dr**2).sum()
+    if (len(node.children)==0) or (node.size**2/r2 < theta2):
+        if r2!=0:
+            leaf.pot += - G * node.m / npy.sqrt(r2)
+    else:
+        for c in node.children:
+            Walk_Pot(c,leaf,theta2)
+            
+
+
+@numba.njit(parallel=True)
+def _calc_potential_direct(m,x,y,z):
+    #G = 6.67e-8
+    pot = npy.zeros(len(m))
+    for j in numba.prange(len(m)):
+        pdist = npy.sqrt( (x-x[j])**2 + (y-y[j])**2 + (z-z[j])**2 )
+        pot[j] = npy.nansum(npy.where( npy.isinf(-G * m / pdist),0,-G * m / pdist ))
+    return pot
+
+
+def _calc_potential_tree(m,x,y,z):
+    theta = 1.1
+    theta2 = theta**2
+    treeorigin = (npy.max((x,y,z),axis=1) + npy.min((x,y,z),axis=1))/2.
+    treewidth = (npy.max((x,y,z)) - npy.min((x,y,z)))
+    leaves = []
+    tree = Node(treewidth,treeorigin,npy.array((x,y,z)).T,m,npy.arange(len(m)),leaves)
+    
+    pot = npy.zeros(len(m))
+    for leaf in leaves:
+        Walk_Pot(tree,leaf,theta2)
+        pot[leaf.id] = leaf.pot
+    return pot
+
+
+def calc_potential(m,x,y,z):
+    if len(m) > 200000:
+        try:
+            return _calc_potential_tree(m,x,y,z)
+        except:
+            return _calc_potential_direct(m,x,y,z)
+    else:
+        return _calc_potential_direct(m,x,y,z)
+    
